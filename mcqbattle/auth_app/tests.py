12573f74d7a86ddeddd67121c4a064eae7899e9b
Aditya Roy Chowdhury <adityaroy2.ar.ar@gmail.com>
2024-06-20 13:08:17 +0530

    my first commit

---
+++ mcqbattle/auth_app/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+# Create your tests here.
+{
+  "first_name": "John",
+  "last_name": "Doe",
+  "email": "john.doe@example.com",
+  "password": "securepassword123"
+}
